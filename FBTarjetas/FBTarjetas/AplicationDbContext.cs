f3609eeb1410cbb05e372a27f665acc5e02d557a
Migueledua17 <31858223+Miguel1415@users.noreply.github.com>
2022-09-12 12:43:13 -0400

    Add files via upload

---
+++ FBTarjetas/FBTarjetas/AplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FBTarjetas.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBTarjetas
+{
+    public class AplicationDbContext: DbContext
+    {
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+        public object TarjetaController { get; internal set; }
+
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ FBTarjetas/FBTarjetas/Models/TarjetaCredito.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBTarjetas.Models
+{
+    public class TarjetaCredito
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public String Titular { get; set; }
+
+        [Required]
+        public String NumeroTarjeta { get; set; }
+
+        [Required]
+        public String FechaExpiracion { get; set; }
+
+        [Required]
+        public String CVV { get; set; }
+    }
+}
